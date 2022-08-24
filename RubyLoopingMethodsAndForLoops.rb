54f7c2bb4acfa954597c8dd2c5a4a53bb4aa237a
Trianan <tristinmanson@gmail.com>
2022-08-23 20:23:54 -0400

    Complete Loops lessons

---
+++ RubyLoopingMethodsAndForLoops.rb
@@ -0,0 +1,37 @@
+#!/usr/bin/ruby 
+
+# RUBY LOOPING METHODS & FOR-LOOPS:
+
+# Ruby for loops iterate through ranges:
+for i in 1..10 do print i.to_s(2) end # do is required only on inline code.
+
+# Nesting and breaks:
+print "\nEnter a number: "
+m = Integer gets
+maxchars, ascii_offset = 26, 96
+charset = ('a'..'z').reject {|chars| chars.ord > m + ascii_offset}
+for i in 1..m
+  break if m > maxchars
+  print "#{i}: "
+  for c in charset # Using char-range.
+    print c, " " # This inserts a space after the character.
+  end
+  print "\n"
+end
+
+# Times method:
+m.times {|i| print i * 11, " "}
+print "\n"
+m.times do print "*_" end
+print "\n"
+
+# Upto method:
+1.upto(m) {|i| if i % 2 == 0 then print i, " " end}
+print "\n"
+
+# Downto method:
+m.downto(1) {|i| unless i % 2 == 0 then print i, " " end}
+print "\n"
+
+
+
--- RubyWhileAndUntilLoops.rb
+++ RubyWhileAndUntilLoops.rb
@@ -1,3 +1,48 @@
 #!/usr/bin/ruby 
 
 # RUBY WHILE & UNTIL LOOPS:
+
+# While loops:
+print "Enter a number: "
+n = Integer gets
+print "\n"
+
+i = 0
+level = []
+while i < n # 'do' can be added here.
+  if i % 2 == 0
+    level << '#'
+  else level << '@'
+  end
+  level.each {|brick| print brick}
+  print "\n"
+  i += 1 # Remember to have an exit condition!
+  break if i > 100000 # Breaks before while statements evals false.
+end
+
+# Until loops:
+i = n
+until i == 0 # Runs until expression is true.
+  level.each {|brick| print brick}
+  print "\n"
+  level.pop
+  i-=1
+end
+
+print "#{i += 7} " until i == (7*n) # Untils may modify statements.
+print "\n"
+
+# Unless statements:
+if n > 60 
+  print "You animal.\n"
+else print "Very considerate.\n"
+end
+
+unless n > 60 # Unless statements invert if-statements.
+  print "Very considerate.\n"
+else print "You animal.\n"
+end # The if and unless statements' output should match.
+
+
+
+
